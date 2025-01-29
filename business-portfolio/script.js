70219b9d0347cd946447db84f2a1e3e87bbc3c92
Angelo Rin <omgk45@gmail.com>
2025-01-28 20:11:25 -0800

    first commit

---
+++ business-portfolio/script.js
@@ -0,0 +1,5 @@
+// Simple form submission handler
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert('Thank you for contacting us! We will get back to you soon.');
+});
