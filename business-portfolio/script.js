723a7b8577e9d713dbddefbe058a15c50bad9c11
Angelo Rin <omgk45@gmail.com>
2025-01-29 07:46:58 -0800

    rework

--- business-portfolio/script.js
+++
@@ -1,5 +0,0 @@
-// Simple form submission handler
-document.getElementById('contact-form').addEventListener('submit', function (e) {
-  e.preventDefault();
-  alert('Thank you for contacting us! We will get back to you soon.');
-});
